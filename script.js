464dd1406b6ada9e5a9cf2fcb2581d9af196d5cd
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-04-17 21:34:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-// your code here
+	let a = 0;
+	let b = 1;
+
+	while(num-- > 0) {
+		let c = a+b;
+		b = a;
+		a = c;
+	}
+
+	return b;
 }
+alert(fibonacci(4));
 
 module.exports = fibonacci;
